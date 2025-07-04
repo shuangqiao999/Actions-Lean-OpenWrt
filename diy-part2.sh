2b8f2548301afd885d62a6cfe3258f7e6b048c7b
shuangqiao999 <94234866+shuangqiao999@users.noreply.github.com>
2025-07-05 06:20:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,5 +11,3 @@
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
 
-#​ OpenAppFilter 
- ​git clone https://​$github​/sbwml/OpenAppFilter --depth=1 package/new/OpenAppFilter
