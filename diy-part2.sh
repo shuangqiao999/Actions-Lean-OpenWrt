e295191838ebfacf3917ee946f974b78d3a1481c
shuangqiao999 <94234866+shuangqiao999@users.noreply.github.com>
2025-07-14 19:44:56 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,4 @@
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
 
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings  #设置密码为空
+# sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings  #设置密码为空
