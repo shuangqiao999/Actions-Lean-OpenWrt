4b8700539918a32822570a4eede966b771d67374
shuangqiao999 <94234866+shuangqiao999@users.noreply.github.com>
2025-08-08 19:42:55 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,21 +10,7 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
-# SmartDNS
-rm -rf feeds/luci/applications/luci-app-smartdns
-git clone https://github.com/lwb1978/luci-app-smartdns package/luci-app-smartdns
-# 替换immortalwrt 软件仓库smartdns版本为官方最新版
-rm -rf feeds/packages/net/smartdns
-# cp -rf ${GITHUB_WORKSPACE}/patch/smartdns package/
-git clone https://github.com/lwb1978/openwrt-smartdns package/smartdns
-# 添加 smartdns-ui
-echo "CONFIG_PACKAGE_luci-app-smartdns_INCLUDE_smartdns_ui=y" >> .config
-echo "CONFIG_PACKAGE_smartdns-ui=y" >> .config
 
-# openssl Enable QUIC and KTLS support
-echo "CONFIG_OPENSSL_WITH_QUIC=y" >> .config
-# echo "CONFIG_OPENSSL_WITH_KTLS=y" >> .config
-echo "SmartDNS 插件切换完成"
 
 git clone https://github.com/muink/openwrt-einat-ebpf.git package/einat-ebpf
 
